0e368d230cc4f5d502e90af1489a5041834a89d0
Fernando R. B <RBFernando99@gmail.com>
2023-01-02 03:05:17 +0100

    Home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function Home() {
+  return (
+    <div>
+      <div>
+        <Container>
+          <Row>
+            <Col>
+              <h1>Home</h1>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
